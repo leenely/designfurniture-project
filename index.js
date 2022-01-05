ff3db65cda25469bfdc96fa91aee9fe954fa0b24
Myakoo <yourflexium@protonmail.com>
2022-01-05 15:53:58 +0600

    Add mobile sliders and burger menu

--- index.js
+++ index.js
@@ -1,35 +1,97 @@
 var swiper = new Swiper(".mySwiper", {
 	direction: "vertical",
 	pagination: {
-	  el: ".swiper-pagination",
-	  clickable: true,
-	  renderBullet: function (index, className) {
-		  return '<span class="' + className + '"> 0' + (index + 1) + '</span>';
-	  },
+		el: ".swiper-pagination",
+		clickable: true,
+		renderBullet: function (index, className) {
+			return '<span class="' + className + '"> 0' + (index + 1) + '</span>';
+		},
 	},
- });
+});
 
- var secondSwiper = new Swiper(".mySwiper2", {
+var secondSwiper = new Swiper(".mySwiper2", {
 	loop: true,
 	pagination: {
 		el: ".swiper-pagination-original",
-	 },
-	navigation: {
-	  nextEl: ".swiper-button-next",
-	  prevEl: ".swiper-button-prev",
 	},
- });
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+});
 
- var thirdSwiper = new Swiper(".mySwiper3", {
+var thirdSwiper = new Swiper(".mySwiper3", {
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+		renderBullet: function (index, className) {
+			switch (index + 1) {
+				case 1:
+					return '<span class="' + className + '">' + 'Стулья' + "</span>";
+				case 2:
+					return '<span class="' + className + '">' + 'Диваны' + "</span>";
+				case 3:
+					return '<span class="' + className + '">' + 'Кресла' + "</span>";
+			}
+		},
+	},
+});
+
+//  Бургер
+(function () {
+
+	let hamburger = {
+		nav: document.querySelector('#nav'),
+		navToggle: document.querySelector('.nav-toggle'),
+
+		initialize() {
+			this.navToggle.addEventListener('click',
+				() => {
+					this.toggle();
+				});
+		},
+
+		toggle() {
+			this.navToggle.classList.toggle('expanded');
+			this.nav.classList.toggle('expanded');
+		},
+	};
+
+	hamburger.initialize();
+
+}());
+
+// Мобильные слайдеры
+
+var mobileFirstSwiper = new Swiper(".mySwiper4", {
+	pagination: {
+		el: ".swiper-pagination",
+		type: "progressbar",
+	},
+});
+
+var mobileSecondSwiper = new Swiper(".mySwiper5", {
+	noSwiping: true,
+	noSwipingClass: 'swiper-slide',
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+		renderBullet: function (index, className) {
+			switch (index + 1) {
+				case 1:
+					return '<span class="' + className + '">' + 'Стулья' + "</span>";
+				case 2:
+					return '<span class="' + className + '">' + 'Диваны' + "</span>";
+				case 3:
+					return '<span class="' + className + '">' + 'Кресла' + "</span>";
+			}
+		},
+	},
+});
+
+var swiper = new Swiper(".mySwiper6", {
 	pagination: {
 	  el: ".swiper-pagination",
-	  clickable: true,
-	  renderBullet: function (index, className) {
-		  switch(index + 1) {
-			  case 1: return '<span class="' + className + '">' + 'Стулья' + "</span>";
-			  case 2: return '<span class="' + className + '">' + 'Диваны' + "</span>";
-			  case 3: return '<span class="' + className + '">' + 'Кресла' + "</span>";
-		  }
-	  },
+	  type: "progressbar",
 	},
  });
