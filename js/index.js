c01870664f2dcdc3f8ac60f78add6245fd7b2448
leenely <yourflexium@protonmail.com>
2025-03-09 18:05:19 +0600

    fix: indention

--- js/index.js
+++ js/index.js
@@ -1,104 +1,101 @@
-var swiper = new Swiper(".mySwiper", {
-	noSwiping: true,
-	noSwipingClass: 'swiper-slide',
-	direction: "vertical",
-	pagination: {
-		el: ".swiper-pagination",
-		clickable: true,
-		renderBullet: function (index, className) {
-			return '<span class="' + className + '"> 0' + (index + 1) + '</span>';
-		},
-	},
-});
+var swiper = new Swiper('.mySwiper', {
+  noSwiping: true,
+  noSwipingClass: 'swiper-slide',
+  direction: 'vertical',
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '"> 0' + (index + 1) + '</span>'
+    },
+  },
+})
 
-var secondSwiper = new Swiper(".mySwiper2", {
-	loop: true,
-	autoplay: {
-		delay: 10000,
-	 },
-	pagination: {
-		el: ".swiper-pagination-original",
-	},
-	navigation: {
-		nextEl: ".swiper-button-next",
-		prevEl: ".swiper-button-prev",
-	},
-});
+var secondSwiper = new Swiper('.mySwiper2', {
+  loop: true,
+  autoplay: {
+    delay: 10000,
+  },
+  pagination: {
+    el: '.swiper-pagination-original',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
 
-var thirdSwiper = new Swiper(".mySwiper3", {
-	noSwiping: true,
-	noSwipingClass: 'swiper-slide',
-	pagination: {
-		el: ".swiper-pagination",
-		clickable: true,
-		renderBullet: function (index, className) {
-			switch (index + 1) {
-				case 1:
-					return '<span class="' + className + '">' + 'Стулья' + "</span>";
-				case 2:
-					return '<span class="' + className + '">' + 'Диваны' + "</span>";
-				case 3:
-					return '<span class="' + className + '">' + 'Кресла' + "</span>";
-			}
-		},
-	},
-});
+var thirdSwiper = new Swiper('.mySwiper3', {
+  noSwiping: true,
+  noSwipingClass: 'swiper-slide',
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    renderBullet: function (index, className) {
+      switch (index + 1) {
+        case 1:
+          return '<span class="' + className + '">' + 'Стулья' + '</span>'
+        case 2:
+          return '<span class="' + className + '">' + 'Диваны' + '</span>'
+        case 3:
+          return '<span class="' + className + '">' + 'Кресла' + '</span>'
+      }
+    },
+  },
+})
 
 //  Бургер
-(function () {
+;(function () {
+  let hamburger = {
+    nav: document.querySelector('#nav'),
+    navToggle: document.querySelector('.nav-toggle'),
 
-	let hamburger = {
-		nav: document.querySelector('#nav'),
-		navToggle: document.querySelector('.nav-toggle'),
+    initialize() {
+      this.navToggle.addEventListener('click', () => {
+        this.toggle()
+      })
+    },
 
-		initialize() {
-			this.navToggle.addEventListener('click',
-				() => {
-					this.toggle();
-				});
-		},
+    toggle() {
+      this.navToggle.classList.toggle('expanded')
+      this.nav.classList.toggle('expanded')
+    },
+  }
 
-		toggle() {
-			this.navToggle.classList.toggle('expanded');
-			this.nav.classList.toggle('expanded');
-		},
-	};
-
-	hamburger.initialize();
-
-}());
+  hamburger.initialize()
+})()
 
 // Мобильные слайдеры
 
-var mobileFirstSwiper = new Swiper(".mySwiper4", {
-	pagination: {
-		el: ".swiper-pagination",
-		type: "progressbar",
-	},
-});
+var mobileFirstSwiper = new Swiper('.mySwiper4', {
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'progressbar',
+  },
+})
 
-var mobileSecondSwiper = new Swiper(".mySwiper5", {
-	noSwiping: true,
-	noSwipingClass: 'swiper-slide',
-	pagination: {
-		el: ".swiper-pagination",
-		clickable: true,
-		renderBullet: function (index, className) {
-			switch (index + 1) {
-				case 1:
-					return '<span class="' + className + '">' + 'Стулья' + "</span>";
-				case 2:
-					return '<span class="' + className + '">' + 'Диваны' + "</span>";
-				case 3:
-					return '<span class="' + className + '">' + 'Кресла' + "</span>";
-			}
-		},
-	},
-});
+var mobileSecondSwiper = new Swiper('.mySwiper5', {
+  noSwiping: true,
+  noSwipingClass: 'swiper-slide',
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    renderBullet: function (index, className) {
+      switch (index + 1) {
+        case 1:
+          return '<span class="' + className + '">' + 'Стулья' + '</span>'
+        case 2:
+          return '<span class="' + className + '">' + 'Диваны' + '</span>'
+        case 3:
+          return '<span class="' + className + '">' + 'Кресла' + '</span>'
+      }
+    },
+  },
+})
 
-var swiper = new Swiper(".mySwiper6", {
-	pagination: {
-	  el: ".swiper-pagination",
-	  type: "progressbar",
-	},
- });
+var swiper = new Swiper('.mySwiper6', {
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'progressbar',
+  },
+})
